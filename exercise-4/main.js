2810b998bba10cd296dd174a30adbc30ed2b640c
Angelo Santos <angelosantos126@gmail.com>
2024-07-30 21:28:54 +0100

    Exercise-4

---
+++ exercise-4/main.js
@@ -0,0 +1,11 @@
+let readlineSync = require("readline-sync");
+
+console.log("Pick a number:");
+let number = readlineSync.question("> ");
+
+let counter = 0;
+
+for (let i = 1; i <= number; i++) {
+    counter = counter + i;
+    console.log(counter);
+}
